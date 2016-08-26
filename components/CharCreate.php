1a1aee230e79b89533504f5ba79b9f032f535717
bimchik86 <d.potapov@infosvyaz.net.ua>
2016-08-26 13:37:02 +0300

    Add component CharCreate

---
+++ components/CharCreate.php
@@ -0,0 +1,21 @@
+<?php namespace Rpg\Mmorpg\Components;
+
+use Cms\Classes\ComponentBase;
+
+class CharCreate extends ComponentBase
+{
+
+    public function componentDetails()
+    {
+        return [
+            'name'        => 'CharCreate Component',
+            'description' => 'No description provided yet...'
+        ];
+    }
+
+    public function defineProperties()
+    {
+        return [];
+    }
+
+}
